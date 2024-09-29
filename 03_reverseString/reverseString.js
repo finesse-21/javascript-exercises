1563952f6f7ff0c9d374bc7ff69bd7b4a4772ed5
finesse-21 <21pepperforreal@gmail.com>
2024-09-30 00:49:13 +0300

    Solve third exercise (garbage solution)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let newString = string.split();
+    let resArray = [];
+    for (let i = 0; i < newString.length; i++) {
+        resArray.push(newString.at(-1 - i));
+    }
+    let temp = "";
+    for (let i = 0; i < resArray.length; i++) {
+        for (let j = 1; j < resArray[i].length + 1; j++) {
+            temp += resArray[i].at(-j);
+        }
+        console.log(temp)
+        resArray.splice(i, 1, temp);
+        temp = "";
+    }
+    let result = resArray.join(" "); 
+    return result;
+};  
 
 // Do not edit below this line
 module.exports = reverseString;
