dd0920758631f3060f5b61646b3b4679deeed23f
finesse-21 <21pepperforreal@gmail.com>
2024-09-30 13:03:21 +0300

    Solve fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let result = 0;
+    //if (second < first) return 'ERROR';
+    if (first < 0 || second < 0) return 'ERROR';
+    if (!Number.isInteger(first) || !Number.isInteger(second)) return 'ERROR';
+    for (let i = Math.min(first,second); i < Math.max(first, second) + 1; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
