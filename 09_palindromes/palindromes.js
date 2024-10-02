bdbb3041e0d96dd2968118391a3f1390657f5977
finesse-21 <21pepperforreal@gmail.com>
2024-10-02 11:59:34 +0300

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let result = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase().split(" ").join('');
+    res = true;
+    for (let i = 0; i < result.length / 2; i++) {
+        if (result[i] != result.at(-1 - i)) {
+            res = false;
+            break
+        }
+    }
+    return res;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
