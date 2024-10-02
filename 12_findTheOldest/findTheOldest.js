9f237f63533ce48865bbd37d1f8f6d3d609a59a5
finesse-21 <21pepperforreal@gmail.com>
2024-10-02 13:08:18 +0300

    Solve final exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.forEach((i) => {
+        if (!i.yearOfDeath) {
+            i.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    return array.reduce((max, old) => (max.yearOfDeath - max.yearOfBirth) > (old.yearOfDeath - old.yearOfBirth) ? max : old);
 };
 
 // Do not edit below this line
