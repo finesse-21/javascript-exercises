f5f151d4f382ab33dcfd718af998e086268bd3ab
finesse-21 <21pepperforreal@gmail.com>
2024-10-02 11:39:05 +0300

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentItem) => {
+    return total += currentItem;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentItem) => {
+    return total *= currentItem;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let result = number;
+	for (let i = 1; i < power; i++) {
+    result *= number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for (let i = 1; i < number + 1; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
